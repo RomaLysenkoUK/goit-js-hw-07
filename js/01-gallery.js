59182db64cb1a9fbc5912d2ed507ff0b0ef9680b
Roman Lysenko <romashkaxxxl@gmail.com>
2022-08-29 23:32:48 +0300

    Update first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,5 +8,38 @@
 
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const newImg = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+        data-source="${image.original}"                      
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
 
+list.insertAdjacentHTML("beforeend", newImg);
+const newGallaryItem = document.querySelector(".gallery__item");
+newGallaryItem.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${e.target.dataset.source}"
+    />
+`);
+
+  instance.show();
+});
+console.dir(newGallaryItem);
 console.log(galleryItems);
+
+//20 строка спросить зачем дата-атрибут если он нигде не используется????
